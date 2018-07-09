254d440f30ce4b63424a26777e758f6f82f1ef00
Ravenstine <tenbitcomb@gmail.com>
2018-07-09 15:27:24 -0700

    Add PouchDB persistence middleware.

--- test/middleware/persistence.js
+++ test/middleware/persistence.js
@@ -1,47 +1,53 @@
 'use strict';
 
 const assert              = require('chai').assert,
-    { entries }           = Object,
+    { keys,
+      entries }           = Object,
       Skill               = require('../../lib/skill'),
       Handler             = require('../../lib/handler'),
       AWS                 = require('aws-sdk-mock'),
       AWS_SDK             = require('aws-sdk'),
+      PouchDB             = require('pouchdb-core'),
       MemoryPersistence   = require('../../lib/middleware/persistence/memory'),
+      PouchDBPersistence  = require('../../lib/middleware/persistence/pouchdb'),
       DynamoDBPersistence = require('../../lib/middleware/persistence/dynamodb'),
       S3Persistence       = require('../../lib/middleware/persistence/s3'),
       AlexaIntentRequest  = require('../mock/alexa/intent.json'),
       AlexaLaunchRequest  = require('../mock/alexa/launch.json');
 
+PouchDB.plugin(require('pouchdb-adapter-memory'));
+PouchDB.plugin(require('pouchdb-erase'));
+
 AWS.setSDKInstance(AWS_SDK);
 
-AWS._fakeDynamoDB = [];
-
-AWS._fakeS3       = {};
+const fakeDynamoDB = [],
+      fakeS3       = {};
 
 AWS.mock('DynamoDB.DocumentClient', 'put', (params, callback) => {
-  AWS._fakeDynamoDB.unshift(params);
+  fakeDynamoDB.unshift(params);
   callback(null, "successfully put item in database");
 });
 
 AWS.mock('DynamoDB.DocumentClient', 'get', (params, callback) => {
-  const item = AWS._fakeDynamoDB.find(i => i.userId === params.Key.userId);
+  const item = fakeDynamoDB.find(i => i.userId === params.Key.userId);
   if(!item) return callback({ code: 'ResourceNotFoundException' });
   callback(null, { Attributes: item.data });
 });
 
 AWS.mock('S3', 'putObject', (params, callback) => {
-  AWS._fakeS3[params.Key] = params.Body;
+  fakeS3[params.Key] = params.Body;
   callback(null, "successfully put item in database");
 });
 
 AWS.mock('S3', 'getObject', (params, callback) => {
-  const Body = AWS._fakeS3[params.Key];
+  const Body = fakeS3[params.Key];
   if(!Body) return callback({ code: 'NoSuchKey' });
   callback(null, { Body });
 });
 
 const middlewares = {
   memory:   MemoryPersistence,
+  pouchdb:  new PouchDBPersistence(new PouchDB('test')),
   s3:       new S3Persistence(AWS_SDK.S3({
     params: {
       Bucket: 'test-bucket'
@@ -61,6 +67,18 @@ describe('middleware', function(){
     entries(middlewares).forEach(([ name, middleware ]) => {
 
       describe(name, function(){
+
+        afterEach((done) => {
+          if(name === 'pouchdb'){
+            middleware.db.erase().then(() => done());
+          } else if(name === 's3'){
+            keys(fakeS3).forEach(key => delete fakeS3[key]); done();
+          } else if(name === 'dynamoDB'){
+            fakeDynamoDB.lengh = 0; done();
+          } else {
+            done();
+          }
+        });
 
         it('persists storage', function(done){
           class TestHandler extends Handler {
